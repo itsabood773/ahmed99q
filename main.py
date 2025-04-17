5d524434e0fa95539b9670e39ac884ce4e3e84df
itsabood773 <142705087+itsabood773@users.noreply.github.com>
2025-04-17 17:40:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     for i in range(10):
         ad = "https://www.profitableratecpm.com/f3ttrgmm?key=c48e66368d4932733432e517cf035cb2"
         api ="QXCup9rVWya171QRYtNHtTqzTxB6lipX"
-        url = f"https://api.webscrapingapi.com/v1?api_key={api}&url={ad}=1&wait_until=networkidle0&device=mobile&wait_for={randint (6000,10000)}"#&country=au"
+        url = f"https://api.webscrapingapi.com/v1?api_key={api}&url={ad}=1&wait_until=networkidle0&device=tablet&wait_for={randint (6000,10000)}&country=au"
 
        # proxy = {"http":f"http://{pro}"}
         thread = threading.Thread(target=make_request, args=(url,))
