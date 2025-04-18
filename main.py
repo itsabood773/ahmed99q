b451e3171353259e63f20e19337468251d348c6d
itsabood773 <142705087+itsabood773@users.noreply.github.com>
2025-04-18 15:07:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def main():
     apis =["JsjLaONt5YGEuVyPv3n5WFEN5ykvZYtI",
         "cgsqAYy0lbi3o2yeX4GTrsVK614RkvN0"]#"f2sGrgZ1BUfKiJ0GHe1rAyZYj1iScjf8"]
     #ad ="https://www.effectiveratecpm.com/ca83bzpx98?key=dee9c6f3171b614287718132222041ad"
-    ad = "https://www.profitableratecpm.com/zhzbtigdvk?key=bfdf77a1bedb6a88e866ad888aa3896b"
+    ad ="https://www.profitableratecpm.com/teuxkz6n?key=d383ed058802a4b1efcdf969672c48d6"
     for api in apis:
         for _ in range(randint (3,5)):
             url = f"https://api.webscrapingapi.com/v2?api_key={api}&url={ad}&country=us&render_js=1"
